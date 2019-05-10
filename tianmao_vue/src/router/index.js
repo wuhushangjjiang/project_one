a3eb000a653db048d13cf72bf2e1467a8a86d508
15378670929 <1284668407@qq.com>
2019-05-10 10:02:09 +0800

    tfhdfhd

--- tianmao_vue/src/router/index.js
+++ tianmao_vue/src/router/index.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
 
-<<<<<<< HEAD
-import routes from './routes'
-=======
 Vue.use(VueRouter)
->>>>>>> 4c42ff5486330c0d6726faef3fe983a416b8589c
 
 export default new VueRouter({
     routes,
